9fc99d08286fdb67b4fdc62602107d8fa4471385
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-08-01 12:05:33 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php 
-
-$insertSql = "INSERT INTO `dictionarytable` (`id`, `dates`, `deutsch`, `english`, `türkçe`) VALUES (NULL, '2024-07-26', 'Hallo', 'Hello', 'Merhaba');";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,20 +7,44 @@ $insertSql = "INSERT INTO `dictionarytable` (`id`, `dates`, `deutsch`, `english`
         <link rel="stylesheet" href="style.css">
 </head>
 <body>
-        <form action="" method="post" style="margin-left: 10%; margin-top: 5%;">
-                <input type="text" name="deutschWort" placeholder="Deutsch Wort">
-                <input type="text" name="englihWord"  placeholder="Enlish Word">
-                <input type="text" name="turkceKelime" placeholder="Türkçe Kelime"><br><br>
-                <Button type="submit" name="saveSumbit" id="saveSumbit" style="margin-left: 12%;" class="glow-on-hover">Save</Button>
-        </form>
-        <?php 
+        <div class="formDiv">
+                <form action="" method="post" style="margin-left: 10%; margin-top: 5%;">
+                        <input type="text" name="deutschWort" placeholder="Deutsch Wort">
+                        <input type="text" name="englihWord"  placeholder="Enlish Word">
+                        <input type="text" name="turkceKelime" placeholder="Türkçe Kelime"><br><br>
+                        <input type="text" name="comment" placeholder="comment" style="width: 90%;"><br><br>
+
+                        <Button type="submit" name="saveSumbit" id="saveSumbit" style="margin: auto" class="glow-on-hover">Save</Button>
+                </form>
+        </div><br>
+
+        <div class="formDiv">
+                
+                <form action="" method="post">
+                        <table border='5' style="text-align:center;">
+                                <thead>
+                                        <th>Deutsch</th>
+                                        <th>English</th>
+                                        <th>Türkçe</th>
+                                </thead>
+                                
+                                <tbody>
+                                <?php
+                                        $sqlC = new sqlController();
+                                        $sqlC->showSql("SELECT * FROM `dictionarytable`"); 
+                                ?>
+                                </tbody>
+                        </table>
+                </form>
+        </div>
+        <?php
         class sqlController{
                 function sqlQueryDone($sql){
                         $servername = "localhost";
                         $username = "root";
                         $password = "";
                         $dbname = "dictionarydb";
-
+        
                         $conn = new mysqli($servername, $username, $password, $dbname);
                         if($conn -> connect_error){
                          die("Fehler beim Herstellen einer Verbindung./Error to connect./Bağlantı hatası.");
@@ -39,8 +57,31 @@ $insertSql = "INSERT INTO `dictionarytable` (`id`, `dates`, `deutsch`, `english`
                                 echo "<h2>Kontrollieren Sie Ihre Eingaben</h2>";
                         }
                  }
-        }
+                 function showSql($sql){
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "dictionarydb";
         
+                        $conn = new mysqli($servername, $username, $password, $dbname); 
+        
+                        $result = $conn->query($sql);
+                        echo "</br>";
+                        if($result->num_rows > 0){
+                                while($row = $result->fetch_assoc()){
+                                        echo "<tr>";
+                                        echo "<td>".$row['deutsch'];
+                                        echo "<td>".$row['english'];
+                                        echo "<td>".$row['türkçe']."</br>";
+                                }
+                        }
+                        else{
+                                echo "Belirlediğiniz Tarihte Kar Kaydıdı bulunmamiş";
+                                return 0;
+                        }
+        
+                }
+        }
         $sqlC = new sqlController();
 
         if(isset($_POST['saveSumbit'])){
@@ -48,12 +89,15 @@ $insertSql = "INSERT INTO `dictionarytable` (`id`, `dates`, `deutsch`, `english`
                 $deWort = $_POST['deutschWort'];
                 $enWord = $_POST['englihWord'];
                 $trKelime = $_POST['turkceKelime'];
+                $comment = $_POST['comment'];
                 if($deWort != '' || $enWord != '' && $trKelime != ''){
-                        $sqlC->sqlQueryDone("INSERT INTO `dictionarytable` (`dates`, `deutsch`, `english`, `türkçe`) VALUES ('$todayDate', '$deWort', '$enWord', '$trKelime');");
+                $sqlC->sqlQueryDone("INSERT INTO `dictionarytable` (`dates`, `deutsch`, `english`, `türkçe`,`comment`) VALUES ('$todayDate', '$deWort', '$enWord', '$trKelime','$comment');");
                 }
                 
-                
-                
+        }
+        else if(isset($_POST['listSumbit'])){
+                header("Refresh:0");
+
         }
         else{
                 echo "";
